2072c5d453d6106b5e5e22c77993898252e1739f
freeton20 <doctor4891@gmail.com>
2022-03-10 22:05:56 +0200

    deploying in different shards

--- MSDeployer.sol
+++ MSDeployer.sol
@@ -3,34 +3,48 @@ pragma ton-solidity 0.58.1;
 import "./MS.sol";
 
 contract MSDeployer is MS{
-    function getShardNumber() public returns(uint8){
 
+    address[] public ms;
+    
+    function getShardNumber(uint _addr) private pure returns(uint8){
+        return uint8(_addr >> 252);
     }
 
-    function deployMS(uint128 amount) public pure returns(address){
+    function deployMS(uint128 amount) public returns(address){
         tvm.accept();
-        TvmCell _code = getMSCode();
-
-        address _addr = new MS{
-            value: amount,
-            code: _code,
-            bounce: true,
-            pubkey: msg.pubkey()
-        }();
-        return _addr;
+        TvmCell _code = getMSCode();    
+        for ((, uint _salt) : getSalts()) {
+            address _addr = new MS{
+                value: amount,
+                code: _code,
+                bounce: true,
+                varInit: {salt: _salt},
+                pubkey: msg.pubkey()
+            }();
+            ms.push(_addr);
+        }       
     }
 
-    function calculateAddress() public pure returns(uint256){
-        tvm.accept();
+    function getSalts() private pure returns(mapping (uint8=>uint256)){      
         TvmCell _code = getMSCode();
-        TvmCell _data = tvm.buildDataInit({
-            contr: MS,
-            varInit: {},
-            pubkey: msg.pubkey()
-        });
-        
-        uint256 _addr = tvm.stateInitHash(tvm.hash(_code), tvm.hash(_data), _code.depth(), _data.depth());
-        return _addr;
+        mapping (uint8=>uint256) salts;
+        uint8 counter = 0; // counter for salts ( only 3 needed )
+        while(counter < 3){
+            uint _salt = rnd.next(now);//salt must be unique
+            TvmCell _data = tvm.buildDataInit({
+                contr: MS,
+                varInit: {salt: _salt},
+                pubkey: msg.pubkey()
+            });
+
+            uint256 _addr = tvm.stateInitHash(tvm.hash(_code), tvm.hash(_data), _code.depth(), _data.depth());
+            uint8 shardNumber = getShardNumber(_addr);           
+            if(salts.add(shardNumber, _salt)){
+                counter++;
+            }
+        }
+
+        return salts;
     }
 
     function getMSDeployerCode() public pure returns(TvmCell){
--- msDeployer.sh
+++ msDeployer.sh
@@ -4,8 +4,7 @@ everdev network default se
 everdev sol compile MSDeployer.sol
 
 echo 'MSDeployer:'
-everdev c deploy MSDeployer.abi.json --value 1000000000000 | grep deployed | xargs
+everdev c deploy MSDeployer.abi.json --value 1000000000000 --data "salt:0" | grep deployed | xargs
 
-everdev c r MSDeployer deployMS -i "amount:100000000000" | grep value0 | xargs
-everdev c r MSDeployer calculateAddress | grep value0 | xargs
-
+everdev c r MSDeployer deployMS -i "amount:100000000000"  --data "salt:0" | grep value0 | xargs
+everdev c l MSDeployer ms  --data "salt:0" 
